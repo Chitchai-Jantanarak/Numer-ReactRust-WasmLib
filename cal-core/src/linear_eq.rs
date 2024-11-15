4c579bd6ae25980a89e4334df9735559bd45f5d8
Chitchai-Jantanarak <s6604062663051@email.kmutnb.ac.th>
2024-11-15 20:20:18 +0700

    Add CG Calc

--- cal-core/src/linear_eq.rs
+++ cal-core/src/linear_eq.rs
@@ -42,6 +42,17 @@ pub(crate) struct LinearIterationResult {
     pub(crate) error: f64
 }
 
+#[derive(Serialize)]
+pub(crate) struct ConjugateResult {
+    pub(crate) iteration: u64,
+    pub(crate) x: Vec<f64>,
+    pub(crate) residual: Vec<f64>,
+    pub(crate) direction: Vec<f64>,
+    pub(crate) error: f64,
+    pub(crate) lambda: f64,
+    pub(crate) alpha: f64
+}
+
 
 
 // wasm conversion JsValue
@@ -95,7 +106,7 @@ pub fn cholesky(mat: Vec<f64>, rows: usize, ans: Vec<f64>) -> JsValue {
 }
 
 #[wasm_bindgen]
-pub fn jacobi(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) -> JsValue {
+pub fn jacobi(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>) -> JsValue {
     match jacobi_core(mat, rows, ans, init) {
         Ok(result) => to_value(&result).unwrap_or_else(|e| JsValue::from_str(&format!("Serialization error: {}", e))),
         Err(e) => JsValue::from_str(&e),
@@ -103,7 +114,7 @@ pub fn jacobi(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) -> JsValue {
 }
 
 #[wasm_bindgen]
-pub fn guass_seidel(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) -> JsValue {
+pub fn guass_seidel(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>) -> JsValue {
     match guass_seidel_core(mat, rows, ans, init) {
         Ok(result) => to_value(&result).unwrap_or_else(|e| JsValue::from_str(&format!("Serialization error: {}", e))),
         Err(e) => JsValue::from_str(&e),
@@ -111,12 +122,23 @@ pub fn guass_seidel(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) -> JsV
 }
 
 #[wasm_bindgen]
-pub fn over_relaxation(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64, omega: f64) -> JsValue {
+pub fn over_relaxation(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>, omega: f64) -> JsValue {
     match over_relaxation_core(mat, rows, ans, init, omega) {
         Ok(result) => to_value(&result).unwrap_or_else(|e| JsValue::from_str(&format!("Serialization error: {}", e))),
         Err(e) => JsValue::from_str(&e),
     }
 }
+
+#[wasm_bindgen]
+pub fn conjugate_gradient(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>) -> JsValue {
+    match cg_core(mat, rows, ans, init) {
+        Ok(result) => to_value(&result).unwrap_or_else(|e| JsValue::from_str(&format!("Serialization error: {}", e))),
+        Err(e) => JsValue::from_str(&e),
+    }
+}
+
+
+
 
 
 // Add implement method
@@ -419,7 +441,7 @@ pub(crate) fn cholesky_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>) -> Result
     })
 }
 
-pub(crate) fn jacobi_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) -> Result<Vec<LinearIterationResult>, String> {
+pub(crate) fn jacobi_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>) -> Result<Vec<LinearIterationResult>, String> {
     
     let cols: usize = mat.len() / rows;
 
@@ -430,7 +452,7 @@ pub(crate) fn jacobi_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) 
     let matrix : Vec<Vec<f64>> = utils::mat_conv2d(&mat, rows);
     let x_size : usize         = ans.len();
 
-    let mut x_old  : Vec<f64>   = vec![init; x_size];
+    let mut x_old  : Vec<f64>   = init.clone();
     let mut x_new  : Vec<f64>   = vec![0.0; x_size];
     let mut result : Vec<LinearIterationResult> = Vec::new();
 
@@ -476,7 +498,7 @@ pub(crate) fn jacobi_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) 
     Ok(result)
 }
 
-pub(crate) fn guass_seidel_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64) -> Result<Vec<LinearIterationResult>, String> {
+pub(crate) fn guass_seidel_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>) -> Result<Vec<LinearIterationResult>, String> {
     
     let cols: usize = mat.len() / rows;
 
@@ -487,7 +509,7 @@ pub(crate) fn guass_seidel_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init:
     let matrix : Vec<Vec<f64>> = utils::mat_conv2d(&mat, rows);
     let x_size : usize         = ans.len();
 
-    let mut x  : Vec<f64>   = vec![init; x_size];
+    let mut x  : Vec<f64>   = init.clone();
     let mut result : Vec<LinearIterationResult> = Vec::new();
 
     // init
@@ -533,7 +555,7 @@ pub(crate) fn guass_seidel_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init:
     Ok(result)
 }
 
-pub(crate) fn over_relaxation_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: f64, omega: f64) -> Result<Vec<LinearIterationResult>, String> {
+pub(crate) fn over_relaxation_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>, omega: f64) -> Result<Vec<LinearIterationResult>, String> {
 
     if omega <= 0.0 || omega >= 2.0 {
         return Err("Relaxation factor omega must be in (0, 2)".to_string());
@@ -548,7 +570,7 @@ pub(crate) fn over_relaxation_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, in
     let matrix : Vec<Vec<f64>> = utils::mat_conv2d(&mat, rows);
     let x_size : usize         = ans.len();
 
-    let mut x  : Vec<f64>   = vec![init; x_size];
+    let mut x  : Vec<f64>   = init.clone();
     let mut result : Vec<LinearIterationResult> = Vec::new();
 
     // init
@@ -590,6 +612,83 @@ pub(crate) fn over_relaxation_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, in
             break;
         }
     }
+
+    Ok(result)
+}
+
+pub(crate) fn cg_core(mat: Vec<f64>, rows: usize, ans: Vec<f64>, init: Vec<f64>) -> Result<Vec<ConjugateResult>, String> {
+    
+    let cols: usize = mat.len() / rows;
+
+    if rows != cols {
+        return Err(format!("Matrix is not square: {} * {}", rows, cols));
+    }
+
+    let matrix : Vec<Vec<f64>> = utils::mat_conv2d(&mat, rows);
+
+    if !utils::is_positive_definite(&matrix) {
+        return Err("Matrix is not positive definite".to_string());
+    }
+
+    let mut result : Vec<ConjugateResult> = Vec::new();
+    
+    // Initialize
+    let mut x         : Vec<f64> = init.clone();
+    let mut residual  : Vec<f64> = utils::vec_sub(&utils::mat_imul_vec(&matrix, &x),&ans);
+    let mut direction : Vec<f64> = utils::scalar_mult(&residual, -1.0);
+    let mut alpha     : f64 = 0.0;
+
+    result.push(ConjugateResult {
+        iteration: 0,
+        x: x.clone(),
+        residual: residual.clone(),
+        direction: direction.clone(),
+        error: 100.0,
+        lambda: -1.0,
+        alpha: -1.0
+    });
+
+    for iter in 0..100 {
+        let a_imul_d = utils::mat_imul_vec(&matrix, &direction);
+
+        // |D^t| * r
+        let lambda_numerator = utils::dot_prod(&direction, &residual);
+        // |D^t| * AD
+        let lambda_denominator = utils::dot_prod(&direction, &a_imul_d);
+
+        // zero-division
+        if lambda_denominator == 0.0 {
+            break;
+        }
+
+        // new lambda --> x --> residual
+        let lambda = lambda_numerator / lambda_denominator;
+        x = utils::vec_add(&x, &utils::scalar_mult(&direction, lambda));
+        residual = utils::vec_sub(&utils::mat_imul_vec(&matrix, &x),&ans);
+
+        let error: f64 = (utils::dot_prod_self(&residual)).sqrt();
+
+        result.push(ConjugateResult {
+            iteration: iter+1,
+            x: x.clone(),
+            residual: residual.clone(),
+            direction: direction.clone(),
+            error,
+            lambda,
+            alpha
+        });
+
+
+        if error < 1e-6 {
+            break;
+        }
+
+        let alpha_numerator = utils::dot_prod(&residual, &a_imul_d);
+        alpha = alpha_numerator / lambda_denominator;
+        direction = utils::vec_add(&utils::scalar_mult(&residual, -1.0), &utils::scalar_mult(&direction, alpha));
+
+    }
+
 
     Ok(result)
 }
